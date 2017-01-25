c2cd4596ce5f3c3ee7a8d66448653be64038a4f7
Victoria Frantsuh <eboko1@gmail.com>
2017-01-25 18:07:58 +0200

    adding Main MAze'

--- src/Maze.java
+++ src/Maze.java
@@ -1,5 +1,26 @@
+import javax.swing.*;
+
 /**
  * Created by Vika on 25.01.2017.
  */
-public class Maze {
+public class Maze extends JFrame {
+    public final int WIDTH = 500;
+    public final int HEIGHT = 400;
+    public Maze(){
+        JFrame frame  = new JFrame();
+        frame.setTitle(" Maze Game ");
+        //
+        frame.setSize(WIDTH,HEIGHT);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public  static void main(String[] args){
+        new Maze();
+    }
+
+
+
+
 }
