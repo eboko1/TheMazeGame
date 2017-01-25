94413c51aa185da5a632d5a277d8c56ab2530295
Victoria Frantsuh <eboko1@gmail.com>
2017-01-25 19:29:57 +0200

    adding paint

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,7 @@
+
+
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,6 +18,15 @@ public class Board  extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
 
+
+
+    public void paint(Graphics g){
+        super.paint(g);//not paint
+
+         g.setColor(Color.red);
+         g.fillRect(45,60,32,32);
     }
 }
--- src/Maze.java
+++ src/Maze.java
@@ -9,7 +9,7 @@ public class Maze extends JFrame {
     public Maze(){
         JFrame frame  = new JFrame();
         frame.setTitle(" Maze Game ");
-        //
+        frame.add(new Board());
         frame.setSize(WIDTH,HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
@@ -23,4 +23,4 @@ public class Maze extends JFrame {
 
 
 
-}
+}3
