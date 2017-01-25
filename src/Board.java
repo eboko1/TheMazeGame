d41f53f823e01bf787bdae791e114fbd3fc95a7c
Victoria Frantsuh <eboko1@gmail.com>
2017-01-25 19:05:13 +0200

    adding Board class

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,20 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Vika on 25.01.2017.
  */
-public class Board {
+public class Board  extends JPanel implements ActionListener {
+    private Timer timer;
 
-    
+    public Board(){
+        timer = new Timer(25,this);
+        timer.start();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
